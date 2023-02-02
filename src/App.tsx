1de426d4194a0e6ca2099fd9de2521fab5a1a1f8
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-02-02 01:24:01 -0400

    add loadNotes function to get notes to local storage

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,37 @@ import NoteEditor from './NoteEditor'
 import storage from './storage'
 import { Note } from './types'
 
-const STORAGE_KEY = "notes"
+const STORAGE_KEY = 'notes'
+
+const loadNotes = () => {
+  const noteIds = storage.get<string[]>(STORAGE_KEY, [])
+  const notes: Record<string, Note> = {}
+  noteIds.forEach((id) => {
+    const note = storage.get<Note>(`${STORAGE_KEY}:${id}`)
+
+    notes[note?.id] = {
+      ...note,
+      updatedAt: new Date(note?.updatedAt),
+    }
+  })
+  return notes
+}
+
+const saveNote = (note: Note) => {
+  const noteIds = storage.get<string[]>(STORAGE_KEY, [])
+  const noteIdsWithoutNote = noteIds.filter((id) => id !== note.id)
+  storage.set(STORAGE_KEY, [...noteIdsWithoutNote, note.id])
+  storage.set(`${STORAGE_KEY}:${note.id}`, note)
+}
 
 function App() {
-  const [notes, setNotes] = useState<Record<string, Note>>({})
+  const [notes, setNotes] = useState<Record<string, Note>>(() =>
+    loadNotes()
+  )
   const [activeNoteId, setActiveNoteId] = useState<string | null>(
     null
   )
   const activeNote = activeNoteId ? notes[activeNoteId] : null
-
-  const saveNote = (note: Note) => {
-    const noteIds = storage.get<string[]>(STORAGE_KEY, [])
-    const noteIdsWithoutNote = noteIds.filter((id) => id !== note.id)
-    storage.set(STORAGE_KEY, [...noteIdsWithoutNote, note.id])
-    storage.set(`${STORAGE_KEY}: ${note.id}`, note)
-  }
 
   const handleChangeNoteContent = (
     noteId: string,
